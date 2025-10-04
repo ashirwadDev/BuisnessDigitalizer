eb792d99c884a1ea49f17ca6a0316185d6070898
Ashirwad Dwivedi <ashirwaddwi5510@gmail.com>
2025-10-04 16:26:15 +0000

    fixed cors

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "*",           // Allow all origins (for testing)
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
